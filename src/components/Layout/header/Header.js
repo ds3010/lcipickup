d400561ce777c1b0b897f19ba64f76728f827232
ds3010 <daniel_seijas3010@hotmail.com>
2023-03-07 16:31:07 -0500

    Issue with hamburger menu - to be fixed

--- src/components/Layout/header/Header.js
+++ src/components/Layout/header/Header.js
@@ -1,15 +1,48 @@
-//import { Navbar } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import { Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-sm navbar-light text-white bg-dark mb-3">
-        HELLO
+      <nav className="navbar navbar-expand-sm navbar-dark bg-secondary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            LCI Pickup Soccer
+          </a>
+          <div className="text-right">
+            <button
+              className="navbar-toggler"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Services
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </nav>
-      {/* <Navbar className="navbar navbar-expand-sm navbar-light bg-light mb-3">
-        HELLO
-      </Navbar> */}
     </>
   );
 };
