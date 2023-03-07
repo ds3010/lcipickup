ba320f5f816017c6eba49d9e48f0a361f67bfee9
ds3010 <daniel_seijas3010@hotmail.com>
2023-03-07 14:20:12 -0500

    Starting the header

--- src/components/Layout/header/Header.js
+++ src/components/Layout/header/Header.js
@@ -1,5 +1,17 @@
+//import { Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-sm navbar-light text-white bg-dark mb-3">
+        HELLO
+      </nav>
+      {/* <Navbar className="navbar navbar-expand-sm navbar-light bg-light mb-3">
+        HELLO
+      </Navbar> */}
+    </>
+  );
+};
 
-}
-
-export default Header
+export default Header;
