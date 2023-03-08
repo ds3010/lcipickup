fc07088f1ff84acd727627dfcb7012611d30cf48
ds3010 <daniel_seijas3010@hotmail.com>
2023-03-07 20:29:23 -0500

    Getting the Hamburger manu to work

--- src/components/Layout/header/Header.js
+++ src/components/Layout/header/Header.js
@@ -1,23 +1,40 @@
 //import { Nav, Navbar } from "react-bootstrap";
+import React, { useState } from "react";
+import './Header.module.css'
+import $ from "jquery";
 
 const Header = () => {
+  //THIS IS ONE WAY TO MAKE THE HAMBURGER MENU WORK:
+  const[expandMenu, setExpandMenu] = useState({showCollapsedMenu: false})
+
+  const toggleMenu = () => {
+    setExpandMenu((previousState) => {
+      return {showCollapsedMenu: !previousState.showCollapsedMenu}
+    })
+  }
+
+  console.log(expandMenu.showCollapsedMenu)
+
+  const showMenu = expandMenu.showCollapsedMenu ? "show" : "";
+  
   return (
     <>
-      <nav className="navbar navbar-expand-sm navbar-dark bg-secondary mb-3">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-secondary mb-3">
         <div className="container">
           <a className="navbar-brand" href="#">
             LCI Pickup Soccer
           </a>
-          <div className="text-right">
             <button
-              className="navbar-toggler"
+              className="navbar-toggler border border-info"
               data-toggle="collapse"
               data-target="#navbarNav"
+              onClick={toggleMenu}
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ml-auto">
+            {/* <div className={"collapse navbar-collapse "} id="navbarNav"> */}
+            <div className={"navbar-collapse collapse " + showMenu} id="navbarNav">
+              <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
                   <a className="nav-link" href="#">
                     Home
@@ -40,7 +57,6 @@ const Header = () => {
                 </li>
               </ul>
             </div>
-          </div>
         </div>
       </nav>
     </>
