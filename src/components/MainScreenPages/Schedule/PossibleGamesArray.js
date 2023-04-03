a5ad603ea8636b29077a6c82ce1f2a2e5aac555c
ds3010 <daniel_seijas3010@hotmail.com>
2023-04-03 14:29:55 -0400

    Implementing Env Variables

--- src/components/MainScreenPages/Schedule/PossibleGamesArray.js
+++ src/components/MainScreenPages/Schedule/PossibleGamesArray.js
@@ -1,14 +1,11 @@
-//1-hour-game: price_1Mro2NA8xt4rMlKr2g4Ve0eQ
-//1-hour-game-extended: price_1Mro42A8xt4rMlKrHIHeGK6h
-
 const PossibleGamesArray = [
   {
-    id: "price_1MrsXNA8xt4rMlKrCvgrVwEz",
+    id: process.env.REACT_APP_PAYMENTID_15,
     title: "15$",
     price: 15.0,
   },
   {
-    id: "price_1MrsXuA8xt4rMlKr0XKraP0C",
+    id: process.env.REACT_APP_PAYMENTID_18,
     title: "18$",
     price: 18.0,
   },
